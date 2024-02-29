a4fb53809bc589c2ce09fead6eb81b3641a95278
Darshak Jani <darshakjani8101@gmail.com>
2024-02-29 16:31:06 +0530

    Implemented home page and footer designs.

---
+++ client/src/components/home/Footer.tsx
@@ -0,0 +1,21 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import { homepageStyles } from "../../styles/homepage-styles";
+
+const Footer = () => {
+  return (
+    <Box sx={homepageStyles.footerContainer}>
+      <Button variant="contained" sx={homepageStyles.footerBtn}>
+        View Articles
+      </Button>
+      <Typography sx={homepageStyles.footerText}>
+        Made with &#x1F498; by DJ
+      </Typography>
+      <Button variant="contained" sx={homepageStyles.footerBtn}>
+        Publish One
+      </Button>
+    </Box>
+  );
+};
+
+export default Footer;
--- client/src/components/home/Homepage.tsx
+++ client/src/components/home/Homepage.tsx
@@ -1,7 +1,53 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import { homepageStyles } from "../../styles/homepage-styles";
+import Footer from "./Footer";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Box sx={homepageStyles.container}>
+      <Box sx={homepageStyles.wrapper}>
+        <Typography sx={homepageStyles.text}>
+          Write and share your blog with millions of people
+        </Typography>
+        <img
+          width="50%"
+          height="50%"
+          //@ts-ignore
+          style={homepageStyles.image}
+          src="/blog.png"
+          alt="Blog"
+        />
+      </Box>
+      <Box sx={homepageStyles.wrapper}>
+        <img
+          width="50%"
+          height="50%"
+          //@ts-ignore
+          style={homepageStyles.image}
+          src="/publish.png"
+          alt="Publish"
+        />
+        <Typography sx={homepageStyles.text}>
+          Write and share your blog with millions of people
+        </Typography>
+      </Box>
+      <Box sx={homepageStyles.wrapper}>
+        <Typography sx={homepageStyles.text}>
+          Write and share your blog with millions of people
+        </Typography>
+        <img
+          width="50%"
+          height="50%"
+          //@ts-ignore
+          style={homepageStyles.image}
+          src="/articles.png"
+          alt="Articles"
+        />
+      </Box>
+      <Footer />
+    </Box>
+  );
 };
 
 export default Homepage;
